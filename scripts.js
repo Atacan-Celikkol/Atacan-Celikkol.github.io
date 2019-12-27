ef02494fdd7b904e711eb30de352e94405940ccf
Atacan <atacan.celikkol@hotmail.com>
2019-12-27 16:43:27 +0300

    Update

--- scripts.js
+++ scripts.js
@@ -1,4 +1,9 @@
 if (screen.width <= 699) {
     const btn = document.getElementById("download");
     btn.click();
+    const body = document.getElementById("container");
+    body.remove();
 }
+// if (screen.width <= 699) {
+//     location.replace('./mobile/index.html');
+// }
